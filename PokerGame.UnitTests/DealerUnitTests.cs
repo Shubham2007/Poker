9c549e93b5eee0550cbd5d8dbd3305b5fd01a240
Shubham Chauhan <shubham.chauhn@gmail.com>
2022-08-15 23:59:53 +0530

    Added few helper methods for unit testing

--- PokerGame.UnitTests/DealerUnitTests.cs
+++ PokerGame.UnitTests/DealerUnitTests.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using PokerGame.Enums;
+using PokerGame.Extensions;
 using PokerGame.Poker;
 using PokerGame.Poker.Interfaces;
+using System.Collections.Generic;
 
 namespace PokerGame.UnitTests
 {
@@ -11,6 +13,8 @@ namespace PokerGame.UnitTests
     {
         private readonly Mock<IDeck> _deck;
         private readonly Dealer _dealer;
+        private HashSet<Suit> suits;
+        private HashSet<CardValue> cardValues;
 
         public DealerUnitTests()
         {
@@ -23,7 +27,7 @@ namespace PokerGame.UnitTests
         public void DealCard_WhenCalled_ReturnCard()
         {
             // Arrange
-            Card deckCard = new(Suit.Club, CardValue.J);
+            Card deckCard = GetRandonCard();
             _deck.Setup(x => x.GetCard()).Returns(deckCard);
 
             // Act
@@ -33,6 +37,33 @@ namespace PokerGame.UnitTests
             Assert.IsNotNull(card);
             Assert.AreEqual(card.Suit, deckCard.Suit);
             Assert.AreEqual(card.Value, deckCard.Value);
+        }
+
+        [TestMethod]
+        public void GetFlop_WhenCalled_ReturnFirst3Cards()
+        {
+            // Arrange
+            _deck.Setup(x => x.GetCard()).Returns(GetRandonCard());
+
+            // Act
+            List<Card> flop = _dealer.GetFlop();
+
+            // Assert
+            Assert.IsNotNull(flop);
+            Assert.AreEqual(3, flop.Count);
+
+        }
+
+        private Card GetRandonCard()
+        {
+            Suit suit = Enum<Suit>.GetRandomValue(exceptList: suits);
+            CardValue value = Enum<CardValue>.GetRandomValue(exceptList: cardValues);
+
+            suits.Add(suit);
+            cardValues.Add(value);
+
+            Card card = new(suit, value);
+            return card;
         }
     }
 }
---
+++ PokerGame/Extensions/EnumExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PokerGame.Extensions
+{
+    /// <summary> Enum Extension Methods </summary>
+    /// <typeparam name="T"> type of Enum </typeparam>
+    public class Enum<T> where T : struct, IConvertible
+    {
+        //It's not a true extension method. It only works because Enum<> is a different type than System.Enum.
+        public static T GetRandomValue(HashSet<T> exceptList)
+        {
+            if (typeof(T) != typeof(Enum))
+                throw new ArgumentException("This method works for enums only");
+
+            Array values = Enum.GetValues(typeof(T));
+
+            if (values.Length <= exceptList.Count)
+                throw new ArgumentException("except list must contain less element than original enum");
+
+            Random random = new();
+            T randomValue;
+            do
+            {
+                randomValue = (T)values.GetValue(random.Next(values.Length));
+
+            } while (exceptList.Contains(randomValue));           
+
+            return randomValue;
+        }
+    }
+}
